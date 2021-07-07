250981d906dd50292b91ff08e37d831b26f63e39
Eliana-Abate <eliana.abate@outlook.it>
2021-07-07 18:34:55 +0200

    snack-4

---
+++ JS-snack-4/js/script.js
@@ -0,0 +1,54 @@
+/*Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro.*/
+
+//! 1. Creo due liste (array) di elementi di lunghezza differente
+//! 2. Creo due variabili per pescare elementi casuali dai due array;
+//! 3. Creo un ciclo WHILE per ciascun array. Stabilisco la condizione di ripetere l'operazione di estrazione random finché la lunghezza dei due array non sarà uguale 
+//! 4. All'interno del ciclo pongo la condizione che gli elementi verranno pushati nell'array SOLO SE presi una volta
+
+
+//! 1 - Array
+var firstArray = ['a', 'b', 'c', 'd', 'f', 'g'];
+console.log('firstarray: ', firstArray);
+
+var secondArray = ['h', 'i'];
+console.log('secondarray: ', secondArray);
+
+
+
+//! 3 - Ciclo per secondArray.length < firstArray.length
+while (secondArray.length < firstArray.length) {
+    //! Operazione 2
+    var randomFirstArray = Math.floor(Math.random() * firstArray.length);
+    console.log('posizione elemento estratto da firstArray: ', randomFirstArray);
+
+    var extractedFirstArray = firstArray[randomFirstArray];
+    console.log('nome elemento estratto da firstArray: ', extractedFirstArray);
+    
+    //! Operazione 4
+   if (!secondArray.includes(extractedFirstArray)) {
+       secondArray.push(extractedFirstArray);
+       console.log('ho inserito: ', extractedFirstArray);
+    }
+}
+
+
+//! 3 - Ciclo per secondArray.length > firstArray.length
+while (secondArray.length > firstArray.length) {
+    //! Operazione 2
+    var randomSecondArray = Math.floor(Math.random() * secondArray.length);
+    console.log('posizione elemento estratto da secondArray: ', randomSecondArray);
+
+    var extractedSecondArray = secondArray[randomSecondArray];
+    console.log('nome elemento estratto da secondArray: ', extractedSecondArray);
+   
+    //! Operazione 4
+   if (!firstArray.includes(extractedSecondArray)) {
+       firstArray.push(extractedSecondArray);
+       console.log('ho inserito: ', extractedSecondArray);
+    }
+}
+
+console.table(firstArray);
+console.table(secondArray);
