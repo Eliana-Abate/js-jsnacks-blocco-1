17550d71ebb3243ab098af1fa9dc9b4d4d20479f
Eliana-Abate <eliana.abate@outlook.it>
2021-07-20 18:20:20 +0200

    edited line 2

--- JS-snack-OGGETTI/JS-snack-1-INPUT-obj/js/script.js
+++ JS-snack-OGGETTI/JS-snack-1-INPUT-obj/js/script.js
@@ -1,12 +1,5 @@
 /*
-SNACK: /
-1. Creare un oggetto palla che abbia le seguenti proprietà.
-  Nome = palla
-  Peso = 10
-2. stampare la palla in pagina*
-3. Attraverso un prompt dare la possibilità all’utente di modificare il peso
-della palla., poi stampare la palla con il peso modificato
-BONUS: stampare la palla tramite una funzione
+SNACK : Invece di modificare il peso con un prompt, proviamo a farlo con un input. Avremo bisogno di un bottone...
 */ 
 
 
